13f602955280fc904f727a966e70e40d35c73762
Joao Pedro Aquino <aqerhack@gmail.com>
2023-08-28 21:24:29 -0300

    add first GET route

--- docker/app/public/index.php
+++ docker/app/public/index.php
@@ -1,7 +1,30 @@
 <?php
+$data = [];
 
-$hw = "Hello World";
-$run = "I run in docker with Nginx";
+//request
+if(isset($_GET['option'])){
+
+    switch ($_GET['option']) {
+        case 'status':
+            $data['status'] = 'SUCCESS';
+            $data['data'] = 'API running OK!';
+            break;
+
+            default:
+            $data['status'] =  'ERROR';
+            break;
+    }
+
+} else {
+    $data['status'] =  'ERROR';
+}
+
+//emitir resposta
+response($data);
 
 
-echo "<p> $hw  $run </p>";
+//construcao da response
+function response($data_response){
+    header("Content-Type:application/json");
+    echo json_encode($data_response);
+}
